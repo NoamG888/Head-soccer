fbae427dd016f133e9fe05a6677d0c1d3f7e52fb
noamg <noam.guez2006@gmail.com>
2023-03-15 17:40:15 +0200

    adding constants and classes buttons and text to project

---
+++ Text.py
@@ -0,0 +1,9 @@
+class Text:
+
+    def __init__(self, text, x_pos, y_pos, size, color, font):
+        self.text = text
+        self.x_pos = x_pos
+        self.y_pos = y_pos
+        self.size = size
+        self.color = color
+        self.font = font
---
+++ constants.py
@@ -0,0 +1,7 @@
+# text constants
+ARIEL = "ariel"
+WHITE = (255, 255, 255)
+
+# screen constants
+WINDOW_WIDTH = 600
+WINDOW_HEIGHT = 800
