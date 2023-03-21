e91e717faf43884ed358d140d4243a249945efcb
noamg <noam.guez2006@gmail.com>
2023-03-21 12:18:37 +0200

    created another square and made the ball avoid touching it fixed ball movement problems and displayed the ball

--- main.py
+++ main.py
@@ -22,19 +22,15 @@ def ball_movement(ball_rect, player_rect1, player_rect2):
         ball_speed[1] = -ball_speed[1]
 
     if pygame.Rect.colliderect(ball_rect, player_rect1):
-        if player_rect1.left < ball_rect.centerx < player_rect1.right:
-            if player_rect1.top <= ball_rect.bottom or player_rect1.bottom >= ball_rect.top:
-                ball_speed[1] = -ball_speed[1]
-        elif player_rect1.top < ball_rect.centery < player_rect1.bottom:
-            if player_rect1.right >= ball_rect.left or player_rect1.left <= ball_rect.right:
-                ball_speed[0] = -ball_speed[0]
+        if player_rect1.right >= ball_rect.centerx >= player_rect1.left:
+            ball_speed[1] = -ball_speed[1]
+        else:
+            ball_speed[0] = -ball_speed[0]
     if pygame.Rect.colliderect(ball_rect, player_rect2):
-        if player_rect2.left < ball_rect.centerx < player_rect2.right:
-            if player_rect2.top <= ball_rect.bottom or player_rect2.bottom >= ball_rect.top:
-                ball_speed[1] = -ball_speed[1]
-        elif player_rect2.top < ball_rect.centery < player_rect2.bottom:
-            if player_rect2.right >= ball_rect.left or player_rect2.left <= ball_rect.right:
-                ball_speed[0] = -ball_speed[0]
+        if player_rect2.right >= ball_rect.centerx >= player_rect2.left:
+            ball_speed[1] = -ball_speed[1]
+        else:
+            ball_speed[0] = -ball_speed[0]
 
 
 def player_movement(event, player1_loc, player2_loc):
@@ -70,13 +66,12 @@ def main():
     pygame.init()
     player1_loc = [700, 300]
     player2_loc = [100, 300]
-    player1_keys = [False, False, False]
-    player2_keys = [False, False, False]
     player1_dir = "up"
     player2_dir = "up"
     screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
     # ball parameters
-    ball = pygame.image.load("C:/Users/noamg/Downloads/walk-0.png")
+    ball = pygame.image.load("images/Untitled-3.png")
+    ball = pygame.transform.scale(ball, (70, 70))
     ball_rect = ball.get_rect()
     global ball_speed
     ball_speed = [2, 2]
@@ -86,12 +81,14 @@ def main():
     running = True
     while running:
         # ball movement
-        ball_movement(ball_rect, square1, square2)
         ball_rect = ball_rect.move(ball_speed)
+        ball_movement(ball_rect, square1, square2)
         pygame.display.flip()
         pygame.time.wait(10)
         # display screen
-        screen.fill(WHITE)
+        field = pygame.image.load("images/מגרש.gif")
+        field = pygame.transform.scale(field, (900, 506.25))
+        screen.blit(field, (0, 0))
         pygame.draw.rect(screen, (0, 150, 0), square1)
         pygame.draw.rect(screen, (150, 0, 0), square2)
         # display ball
