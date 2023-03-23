d8fb87f6e79d852c8edd182fa859f9763a11b704
noamg <noam.guez2006@gmail.com>
2023-03-23 22:03:52 +0200

    i added scoring and made the goals like walls

--- Text.py
+++ Text.py
@@ -1,3 +1,6 @@
+import pygame
+
+
 class Text:
 
     def __init__(self, text, x_pos, y_pos, size, color, font):
@@ -7,3 +10,9 @@ class Text:
         self.size = size
         self.color = color
         self.font = font
+
+    def display(self, screen):
+        text_font = pygame.font.SysFont(self.font, self.size)
+        text_surf = text_font.render(str(self.text), True, self.color)
+        text_rect = text_surf.get_rect(center=(self.x_pos, self.y_pos))
+        screen.blit(text_surf, text_rect)
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from Text import Text
 
 
 # displays an image on the screen
@@ -10,27 +11,38 @@ def add_image(image_path, x_position, y_position, width, height):
 
 
 # displays images on the screen using the "add_image" function
-def load_images():
-    pass
+def goal_check(ball_rect, left_goal_rect, right_goal_rect, goal_counter1, goal_counter2):
+    if ball_rect.right <= left_goal_rect.right and ball_rect.top >= left_goal_rect.bottom:
+        goal_counter1.text += 1
+        ball_rect = pygame.Rect((WINDOW_WIDTH-70) // 2, 50, 70, 70)
+        return ball_rect
+    if ball_rect.left >= right_goal_rect.left and ball_rect.top >= right_goal_rect.bottom:
+        goal_counter2.text += 1
+        ball_rect = pygame.Rect((WINDOW_WIDTH-50) // 2, 50, 70, 70)
+        return ball_rect
+    return ball_rect
+
+
+def walls_ball_movement(ball_rect):
+    if ball_rect.left < 0 or ball_rect.right > WINDOW_WIDTH:
+        ball_speed[0] = -ball_speed[0]
+    if ball_rect.top < 0 or ball_rect.bottom > WINDOW_HEIGHT - 50:
+        ball_speed[1] = -ball_speed[1]
 
 
 # in charge of the ball's movement after a player hits it
-def ball_movement(ball_rect, player_rect1, player_rect2):
-    if ball_rect.left < 0 or ball_rect.right > WINDOW_WIDTH:
-        ball_speed[0] = -ball_speed[0]
-    if ball_rect.top < 0 or ball_rect.bottom > WINDOW_HEIGHT:
-        ball_speed[1] = -ball_speed[1]
-
-    if pygame.Rect.colliderect(ball_rect, player_rect1):
-        if player_rect1.right >= ball_rect.centerx >= player_rect1.left:
+def ball_movement(ball_rect, player_rect):
+    if pygame.Rect.colliderect(ball_rect, player_rect):
+        if player_rect.right >= ball_rect.centerx >= player_rect.left and player_rect.bottom >= ball_rect.centery >= player_rect.top:
+            ball_speed[0] = -ball_speed[0]
             ball_speed[1] = -ball_speed[1]
+        elif player_rect.right >= ball_rect.centerx >= player_rect.left:
+            ball_speed[1] = -ball_speed[1]
+        elif player_rect.bottom >= ball_rect.centery >= player_rect.top:
+            ball_speed[0] = -ball_speed[0]
         else:
             ball_speed[0] = -ball_speed[0]
-    if pygame.Rect.colliderect(ball_rect, player_rect2):
-        if player_rect2.right >= ball_rect.centerx >= player_rect2.left:
             ball_speed[1] = -ball_speed[1]
-        else:
-            ball_speed[0] = -ball_speed[0]
 
 
 def player_movement(event, player1_loc, player2_loc):
@@ -64,35 +76,49 @@ def jump(direction, loc):
 def main():
     global screen
     pygame.init()
-    player1_loc = [700, 300]
-    player2_loc = [100, 300]
-    player1_dir = "up"
-    player2_dir = "up"
+    player1_loc = [700, 375]
+    player2_loc = [100, 375]
+    # player1_dir = "up"
+    # player2_dir = "up"
     screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
     # ball parameters
     ball = pygame.image.load("images/Untitled-3.png")
-    ball = pygame.transform.scale(ball, (70, 70))
+    # goals = pygame.image.load("images/football_gate.gif")
+    # goals = pygame.transform.scale(goals, (WINDOW_WIDTH, WINDOW_HEIGHT))
+    ball = pygame.transform.scale(ball, (50, 50))
     ball_rect = ball.get_rect()
     global ball_speed
-    ball_speed = [2, 2]
+    ball_speed = [3, 3]
     square1 = pygame.Rect(player1_loc[0], player1_loc[1], 100, 100)
     square2 = pygame.Rect(player2_loc[0], player2_loc[1], 100, 100)
+    left_goal_top = pygame.Rect(0, 270, 200, 5)
+    right_goal_top = pygame.Rect(750, 270, 202, 5)
+    goal_counter1 = Text(0, 100, 40, 80, WHITE, ARIEL)
+    goal_counter2 = Text(0, 800, 40, 80, WHITE, ARIEL)
     # run loop
     running = True
     while running:
+        ball_rect = goal_check(ball_rect, left_goal_top, right_goal_top, goal_counter1, goal_counter2)
         # ball movement
         ball_rect = ball_rect.move(ball_speed)
-        ball_movement(ball_rect, square1, square2)
+        walls_ball_movement(ball_rect)
+        ball_movement(ball_rect, square1)
+        ball_movement(ball_rect, square2)
+        ball_movement(ball_rect, left_goal_top)
+        ball_movement(ball_rect, right_goal_top)
+        # screen.blit(goals, (0, 0))
         pygame.display.flip()
-        pygame.time.wait(10)
+        pygame.time.wait(3)
         # display screen
         field = pygame.image.load("images/מגרש.gif")
-        field = pygame.transform.scale(field, (900, 506.25))
+        field = pygame.transform.scale(field, (WINDOW_WIDTH, WINDOW_HEIGHT))
         screen.blit(field, (0, 0))
         pygame.draw.rect(screen, (0, 150, 0), square1)
         pygame.draw.rect(screen, (150, 0, 0), square2)
         # display ball
         screen.blit(ball, ball_rect)
+        goal_counter1.display(screen)
+        goal_counter2.display(screen)
         # checks for events
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
